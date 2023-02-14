ea5a988a7f12f2599647cadbf22f38821d946c86
Eva Camino <caminoeva1@gmail.com>
2023-02-13 19:05:00 -0700

    last js

--- Develop/script.js
+++ Develop/script.js
@@ -24,6 +24,38 @@ $(function () {
       function colorBlocks() {
         $(".time-block").each(function () {
           var blockId = $(this).attr("id");
+          blockId = blockId.substring(5)
+          blockId = parseInt(blockId)
+          console.log(blockId)
+          console.log(blockId,currentHour)
+          if (currentHour > blockId) {
+            $(this).addClass("past");
+          } else if (currentHour === blockId) {
+            $(this).addClass("present");
+          } else {
+            $(this).addClass("future");
+          }
+        });
+      }
+      colorBlocks()
+    });
+    //   // TODO: Add code to apply the past, present, or future class to each time
+    //   // block by comparing the id to the current hour. HINTS: How can the id
+    //   // attribute of each time-block be used to conditionally add or remove the
+    //   // past, present, and future classes? How can Day.js be used to get the
+    //   // current hour in 24-hour time?
+    
+    //   if
+    //   //
+    //   // TODO: Add code to get any user input that was saved in localStorage and set
+    //   // the values of the corresponding textarea elements. HINT: How can the id
+    //   // attribute of each time-block be used to do this?
+    //   //
+    //   // TODO: Add code to display the current date in the header of the page.
+    //   \
+    
+    // });
+    
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
